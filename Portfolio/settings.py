793b0cef9179e01de9051d1dab71757f91f94c47
tanjimkhan <tanjim.ak49@gmail.com>
2022-12-09 04:18:27 +0000

    a bunch of stuff

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-from django.conf.global_settings import STATIC_ROOT
+# from django.conf.global_settings import STATIC_ROOT
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -121,11 +121,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Portfolio/static/')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'Portfolio/static/')
+# ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
 from .models import Project
-
+import mimetypes
+import os
+from django.http.response import HttpResponse
 
 def home(request):
     projects = Project.objects
     return render(request, "projects/home.html", {'projects': projects})
+
+def download(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'Tanjim-Ahmed-Khan-Resume.pdf'
+    # Define the full file path
+    filepath = BASE_DIR + '/media/docs/' + filename
+    # Open the file for reading content
+    path = open(filepath, 'rb')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
